d598668890ab4e4a7f38fa75007d33cfb7336057
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-14 14:46:57 -0400

    Removed extra searchbar.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Header from './components/Header'
 import MainContianer from './components/MainContainer';
-import SearchForm from './components/SearchForm';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchForm />
       <MainContianer/>
     </div>
   );
