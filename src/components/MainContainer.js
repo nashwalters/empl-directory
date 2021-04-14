b30cde4157f4378a5108543714e7e26b70eb49b2
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-14 14:23:51 -0400

    Modified Main container.

---
+++ src/components/MainContainer.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import Search from "./Search";
+import Table from "./Table";
+import API from "../utils/API";
+import _get from "lodash/get";
+
+class MainContianer extends Component {
+  state = {
+    results: [],
+    toggle: true,
+    searchInput: "",
+  };
+  
+  renderEmplList = () => {
+    API.search()
+      .then((res) => {
+        this.setState({ results: res.data.results });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  componentDidMount() {
+    this.renderEmplList();
+  }
+
+  sortByName = (param) => {
+    const sortedEmployeeList = this.state.results.sort((emp1, emp2) =>
+      this.state.toggle
+        ? _get(emp1, param).localeCompare(_get(emp2, param))
+        : _get(emp2, param).localeCompare(_get(emp1, param))
+    );
+    this.setState({ results: sortedEmployeeList, toggle: !this.state.toggle });
+  };
+
+  searchFilter = (event) => {
+    // const newEmployeeList = this.state.results.filter(employee => employee.name.first === input)
+    this.setState({ searchInput: event.target.value.toLowerCase() });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Search search={this.searchFilter} />
+        <Table
+          searchInput={this.state.searchInput}
+          results={this.state.results}
+          handleDelete={this.handleDelete}
+          sortByName={this.sortByName}
+        />
+      </div>
+    );
+  }
+}
+
+export default MainContianer;
